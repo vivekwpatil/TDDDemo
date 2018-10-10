f6ecab62cbdc3f9d3734e7f13480ddc2004c80eb
Vivek Patil <patil.vivek@ing.be>
2018-10-10 15:09:24 +0200

    TDDDemo: first commit with integration test for /sum endpoint

--- src/test/java/com/java/engineering/tdddemo/TdddemoApplicationTests.java
+++ src/test/java/com/java/engineering/tdddemo/TdddemoApplicationTests.java
@@ -6,23 +6,56 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { ApplicationFilterConfig.class })
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {TdddemoApplication.class})
 @WebAppConfiguration
 public class TdddemoApplicationTests {
 
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = wac.getServletContext();
+
+        Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(wac.getBean("sumController"));
+    }
+
+    @Test
+    public void givenSumAPIWithPostAndFormData_whenMockMVC_thenResponseOK() throws Exception  {
+        this.mockMvc.perform(post("/sum").param("number1", "1")
+            .param("number2", "3")).andDo(print()).andExpect(status().isOk());
+
+            /*.andExpect(content().contentType("application/json;charset=UTF-8"))
+            .andExpect(jsonPath("$.sum").value("5"));*/
+    }
 
 
 
