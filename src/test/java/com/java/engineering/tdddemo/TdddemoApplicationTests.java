b68a8abcc8a5bbc29580705e0761713503ec5b1d
Vivek Patil <patil.vivek@ing.be>
2018-10-10 14:47:21 +0200

    TDDDemo: first commit

---
+++ src/test/java/com/java/engineering/tdddemo/TdddemoApplicationTests.java
@@ -0,0 +1,29 @@
+package com.java.engineering.tdddemo;
+
+import org.apache.catalina.core.ApplicationFilterConfig;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { ApplicationFilterConfig.class })
+@WebAppConfiguration
+public class TdddemoApplicationTests {
+
+
+
+
+}
