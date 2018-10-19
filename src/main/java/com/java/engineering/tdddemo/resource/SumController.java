bdd7debb41a2b93c3098f6c091c756ce3887b5e7
Vivek Patil <patil.vivek@ing.be>
2018-10-19 15:00:19 +0200

    TDDDemo: fixed implementation for :empty object returns zero

--- src/main/java/com/java/engineering/tdddemo/resource/SumController.java
+++ src/main/java/com/java/engineering/tdddemo/resource/SumController.java
@@ -3,6 +3,7 @@ package com.java.engineering.tdddemo.resource;
 import com.java.engineering.tdddemo.model.SumRequest;
 import com.java.engineering.tdddemo.model.SumResponse;
 import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,7 +15,8 @@ public class SumController {
     @PostMapping(value = "/sum", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     SumResponse sum(@RequestBody SumRequest sumRequest) {
-        return new SumResponse(sumRequest.getNumber1() + sumRequest.getNumber2());
+
+        return new SumResponse((StringUtils.isEmpty(sumRequest.getNumber1())?0 :sumRequest.getNumber1()) +( StringUtils.isEmpty(sumRequest.getNumber2())?0 :sumRequest.getNumber2()));
     }
 
 
