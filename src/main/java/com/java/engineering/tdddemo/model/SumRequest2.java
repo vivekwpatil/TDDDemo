df34742e83d5cbee8921d831979221901de41abc
Vivek Patil <patil.vivek@ing.be>
2018-10-26 14:33:03 +0200

    TDDDemo: added implementation for : sizee check bwtween 0 to 999

--- src/main/java/com/java/engineering/tdddemo/model/SumRequest2.java
+++ src/main/java/com/java/engineering/tdddemo/model/SumRequest2.java
@@ -1,10 +1,12 @@
 package com.java.engineering.tdddemo.model;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 public class SumRequest2 implements Serializable {
     @NotNull(message = "must not be null")
+    @Pattern( regexp = "^0*([0-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9])$", message = "Size must be between 0 and 999")
     private String number1;
     @NotNull(message = "must not be null")
     private String number2;
